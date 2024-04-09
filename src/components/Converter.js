da724491a74869ce153d17bbedb3afe627d99a18
Gustavo Santana <guga.hrs14@gmail.com>
2024-04-08 23:33:57 -0300

    atualizando local do adsense

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { faMoneyBillAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Converter.css';
-
 
 function Converter({
   currencies,
@@ -15,19 +14,34 @@ function Converter({
   convertedAmount,
   convertCurrency
 }) {
+  const [exchangeRates, setExchangeRates] = useState({});
 
-    const redirectToCurrencyCodesPage = () => {
-        window.location.href = 'https://pt.iban.com/currency-codes';
-      };
+  // Função para buscar as taxas de câmbio da moeda selecionada
+  const fetchExchangeRates = async () => {
+    try {
+      const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${inputCurrency}`);
+      const data = await response.json();
+      setExchangeRates(data.rates);
+    } catch (error) {
+      console.error('Erro ao buscar taxas de câmbio:', error);
+    }
+  };
+
+  // Atualiza as taxas de câmbio sempre que a moeda de entrada for alterada
+  useEffect(() => {
+    fetchExchangeRates();
+  }, [inputCurrency]);
 
   return (
     <div className="converter">
+      <div className="ad-container">
+        {/* Código de anúncio do Google AdSense para o espaço lateral */}
+      </div>
       <h2 className="title">Conversor de Moedas</h2>
       <div className="floating-coins">
         <div className="coin"></div>
         <div className="coin"></div>
         <div className="coin"></div>
-        {/* Adicione mais moedas flutuantes conforme necessário */}
       </div>
       <div className="input-field">
         <input
@@ -69,7 +83,6 @@ function Converter({
         </select>
       </div>
       <button onClick={convertCurrency} className="convert-button">Converter</button>
-      <button onClick={redirectToCurrencyCodesPage} className='cash-code'>Ver Códigos das Moedas</button>
     </div>
   );
 }
